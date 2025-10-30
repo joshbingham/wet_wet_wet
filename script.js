d56b2c34559d3c43eb4c6852bbae4eae6d822997
Josh Bingham <joshuabingham@hotmail.co.uk>
2025-10-30 14:41:21 +0000

    Change type to module

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-// API key
-require('dotenv').config({ path: './weather.env' });
-const apiKey = process.env.WEATHERSTACK_API_KEY;
-
-const fetch = require('node-fetch'); // only needed if using Node.js <18
+import { WEATHERSTACK_API_KEY as apiKey } from './config.js';
 
 const city = "London";
 const url = `http://api.weatherstack.com/current?access_key=${apiKey}&query=${city}`;
@@ -13,4 +9,5 @@ fetch(url)
     console.log(`Weather in ${data.location.name}: ${data.current.temperature}°C`);
     console.log(`Conditions: ${data.current.weather_descriptions[0]}`);
   })
-  .catch(err => console.error("Error fetching weather:", err));
+  .catch(err => console.error(err));
+
